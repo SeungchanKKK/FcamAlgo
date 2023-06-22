444a27f27e879e5b7778db676d6856ec2328d579
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-06-22 12:34:17 +0900

    6/22 링크드리스트-3

--- src/main/java/com/example/fastcam/DoubleLinkedList.java
+++ src/main/java/com/example/fastcam/DoubleLinkedList.java
@@ -1,54 +1,55 @@
 package com.example.fastcam;
 
 public class DoubleLinkedList<T>{
-    public Node<T>head =null;
-    public Node<T>tail =null;
-
+    Node<T>head =null;
+    Node<T>tail = null;
     public class Node<T>{
         T data;
         Node<T>prev = null;
         Node<T>next = null;
 
         public Node(T data){
-            this.data=data;
+            this.data= data;
         }
     }
 
     public void addNode(T data){
-        if(this.head==null){
-            this.head= new Node<T>(data);
-            this.tail= this.head;
-        }else{
+        if (this.head==null){
+            this.head=new Node<>(data);
+            this.tail = this.head;
+        }else {
             Node<T>node = this.head;
-            while(node.next !=null){
+            while (node.next!=null){
                 node=node.next;
             }
-            node.next=new Node<T>(data);
+            node.next=new Node<>(data);
             node.next.prev=node;
             this.tail=node.next;
         }
     }
 
     public void printAll(){
-        if(this.head!=null){
-            Node<T>node =this.head;
-            System.out.println(node.data);
-            while(node.next!=null){
-                node=node.next;
+        if(head==null){
+            System.out.println("null");
+        }else {
+            Node<T> node = this.head;
+            while (node.next!=null){
                 System.out.println(node.data);
+                node=node.next;
             }
+            System.out.println(node.data);
         }
     }
 
-    public T searchFromHead( T isData){
+    public T searchFromHead(T isData){
         if(this.head==null){
             return null;
         }else {
             Node<T>node = this.head;
             while (node!=null){
-                if(node.data==isData){
+                if (node.data==isData){
                     return node.data;
-                } else {
+                }else {
                     node = node.next;
                 }
             }
@@ -56,19 +57,30 @@ public class DoubleLinkedList<T>{
         }
     }
 
-    public T searchFromTail(T isData) {
-        if (this.head == null) {
+    public T searchFromTail(T isData){
+        if (this.tail==null){
             return null;
-        } else {
-            Node<T> node = this.tail;
-            while (node != null) {
-                if (node.data == isData) {
+        }else {
+            Node<T>node = this.tail;
+            while (node!=null){
+                if (node.data==isData){
                     return node.data;
-                } else {
-                    node = node.next;
+                }else {
+                    node= node.prev;
                 }
             }
             return null;
         }
+    }
+
+    public static void main(String[] args) {
+        DoubleLinkedList<Integer>MyLinkedList = new DoubleLinkedList<>();
+        MyLinkedList.addNode(1);
+        MyLinkedList.addNode(2);
+        MyLinkedList.addNode(3);
+        MyLinkedList.addNode(4);
+        MyLinkedList.addNode(5);
+
+        MyLinkedList.printAll();
     }
 }
