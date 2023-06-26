43cae52297bb5c446f434b2d2d1f79c5b6dc4dc4
seungchanKKK <eccsck@gmail.com>
2023-06-26 22:32:55 +0900

    6/26 ch17 그래프이해와 자료구조

--- src/main/java/com/example/fastcam/BinarySearch.java
+++ src/main/java/com/example/fastcam/BinarySearch.java
@@ -3,26 +3,27 @@ package com.example.fastcam;
 import java.util.ArrayList;
 
 public class BinarySearch {
-    public boolean searchFunc(ArrayList<Integer> dataList, Integer searchItem){
-        if(dataList.size()==1 && searchItem == dataList.get(0)){
+    public  boolean searchFunc(ArrayList<Integer>dataList,Integer searchItem){
+        if (dataList.size() == 1 && searchItem == dataList.get(0)) {
             return true;
         }
-        if(dataList.size()==1 && searchItem != dataList.get(0)){
+        if (dataList.size() == 1 && searchItem != dataList.get(0)) {
             return false;
         }
-        if(dataList.size()==0){
+        if (dataList.size() == 0) {
             return false;
         }
 
         int medium = dataList.size()/2;
 
-        if(searchItem == dataList.get(medium)){
+
+        if (searchItem == dataList.get(medium)) {
             return true;
-        }else {
-            if(searchItem<dataList.get(medium)){
-                return this.searchFunc(new ArrayList<>(dataList.subList(0,medium)),searchItem);
-            }else {
-                return this.searchFunc(new ArrayList<>(dataList.subList(medium,dataList.size())),searchItem);
+        } else {
+            if (searchItem < dataList.get(medium)) {
+                return this.searchFunc(new ArrayList<Integer>(dataList.subList(0, medium)), searchItem);
+            } else {
+                return this.searchFunc(new ArrayList<Integer>(dataList.subList(medium, dataList.size())), searchItem);
             }
         }
     }
