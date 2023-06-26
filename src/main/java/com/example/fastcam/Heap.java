e8d3de6336b59c925dff7a82543314dcbe899158
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-06-26 11:18:11 +0900

    6/26 힙-2

--- src/main/java/com/example/fastcam/Heap.java
+++ src/main/java/com/example/fastcam/Heap.java
@@ -5,61 +5,45 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Heap {
-    public static void main(String[] args){
-        Heap heap = new Heap(15);
-        heap.insert(10);
-        heap.insert(8);
-        heap.insert(5);
-        heap.insert(4);
-        heap.insert(20);
-        System.out.println(heap.heapArray);
-    }
-    public ArrayList<Integer>heapArray =null;
+    public ArrayList<Integer>heapArray=null;
 
-    public Heap(Integer data){
+    public Heap (Integer data){
         heapArray = new ArrayList<>();
 
         heapArray.add(null);
         heapArray.add(data);
     }
-    public boolean move_up(Integer inserted_idx) {
-        if(inserted_idx<=1){
-            return false;
-        }
-        Integer parent_idx = inserted_idx/2;
-        if(this.heapArray.get(inserted_idx)>this.heapArray.get(parent_idx)){
-            return true;
-        }else {
-            return false;
-        }
-    }
+
     public boolean insert(Integer data){
         Integer inserted_idx,parent_idx;
-
         if(heapArray==null){
-            heapArray = new ArrayList<Integer>();
+            heapArray = new ArrayList<>();
 
             heapArray.add(null);
             heapArray.add(data);
             return true;
         }
-
         this.heapArray.add(data);
-        inserted_idx = this.heapArray.size()-1;
-
-        while (this.move_up(inserted_idx)){
-            parent_idx = inserted_idx/2;
-            Collections.swap(this.heapArray,inserted_idx,parent_idx);
-            inserted_idx = parent_idx;
+        inserted_idx=this.heapArray.size()-1;
+        while (heapArray.get(inserted_idx)>heapArray.get(inserted_idx/2)&&inserted_idx>1){
+            parent_idx=inserted_idx/2;
+            Collections.swap(heapArray,inserted_idx,parent_idx);
+            inserted_idx=parent_idx;
+            if(inserted_idx==1){
+                break;
+            }
         }
         return true;
     }
 
-    public Integer pop(){
-        if(this.heapArray==null){
-            return null;
-        }else {
-            return this.heapArray.get(1);
-        }
+    public static void main(String[] args) {
+        Heap heapTest = new Heap(15);
+        heapTest.insert(10);
+        heapTest.insert(8);
+        heapTest.insert(5);
+        heapTest.insert(4);
+        heapTest.insert(20);
+
+        System.out.println(heapTest.heapArray);
     }
 }
